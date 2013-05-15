48ac6ae1b139589b46f1b5bb25afd9aa5f6086dc
Thilo-Alexander Ginkel <thilo@ginkel.com>
2013-05-15 10:18:57 +0200

    Remove incorrect whitespace from default config

--- config/DEFAULT.php
+++ config/DEFAULT.php
@@ -5,7 +5,7 @@ $config['author_url']	= "Please add your own url here";
 $config['url']			= "http://url.of.the.feed/fedd.rss"; // just add the url to rss feed
 $config['base_url']		= "http://normal.url.of.the.website"; // add the normal url of the page
 $config['content']		= array("#(.*)#Uis", 1);
-$config['search	']		= array("#<ads>.*<\/ads>#Uis",);
+$config['search']		= array("#<ads>.*<\/ads>#Uis",);
 $config['replace']		= array("");
 
 ?>
